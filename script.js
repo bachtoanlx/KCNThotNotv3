98dd32529b4fbc2f370476b1b9cbdfa4140e4401
bachtoanlx <167489931+bachtoanlx@users.noreply.github.com>
2025-03-14 10:08:33 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -268,3 +268,74 @@ async function sendDataWithRetry(date, user, c_ty, ghi_chu, fileData, fileName, 
 
 
 
+
+
+
+  
+
+/*Xử lý hành động gửi dữ liệu vào GG Sheet (mẫu dự phòng)
+  function submitForm_2() {
+    // Lấy thông tin người dùng đã đăng nhập từ localStorage
+    var loggedInUser = localStorage.getItem('loggedInUser');
+  
+    if (!loggedInUser) {
+        // Nếu người dùng chưa đăng nhập, hiển thị modal đăng nhập
+        $('#loginModal').modal('show');
+        return; // Dừng hàm submitForm ở đây để ngăn việc gửi dữ liệu khi chưa đăng nhập
+    }
+  
+    // Hiển thị modal loading
+    $('#loadingModal').modal('show');
+  
+    var c_ty = document.getElementById('c_ty').value;
+    var ngay_nghi = document.getElementById('ngay_nghi').value || getCurrentDate();
+    var ghi_chu = document.getElementById('ghi_chu').value;
+  
+    if (!c_ty || !ngay_nghi) {
+        $('#errorModal').modal('show'); // Hiển thị modal lỗi
+        $('#loadingModal').modal('hide'); // Ẩn modal loading
+        return;
+    }
+  
+    var formData = {
+        sheetName: 'Sheet2',
+        user: loggedInUser, // Thêm thông tin người dùng đăng nhập vào đây
+        c_ty: c_ty,
+        ngay_nghi: ngay_nghi,
+        ghi_chu: ghi_chu  
+    };
+  
+    console.log(formData);
+    fetch('https://script.google.com/macros/s/AKfycbzg9lkoiVZeVE7kXcDV7gVa7lijeXjP-tAHukkv4lac5fXNzBJNOLYQEiSyu9cVgkmy/exec', {
+        method: 'POST',
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if (data.result === 'success') {
+            // Hiển thị modal thành công
+            $('#successModal').modal('show');
+  
+            // Tự động đóng modal sau 2 giây
+            setTimeout(function () {
+                $('#successModal').modal('hide');
+            }, 2000);
+  
+            // Reset form
+            document.getElementById('registrationForm').reset();
+        } else {
+            // Hiển thị modal lỗi
+            $('#errorModal').modal('show');
+        }
+    })
+    .catch(error => {
+        console.error('Đã xảy ra lỗi:', error);
+        $('#errorModal').modal('show'); // Hiển thị modal lỗi
+    })
+    .finally(() => {
+        // Ẩn modal loading sau khi hoàn thành
+        $('#loadingModal').modal('hide');
+    });
+  }
+  */
